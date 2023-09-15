7126b7dd243ec2d6a8401253c384f554812f9b52
Barros <eduardoscb@fractal.dev.br>
2023-09-15 01:25:47 -0300

    initial commit

---
+++ components/action-tooltip.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { 
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider
+} from "@/components/ui/tooltip"
+import React from "react";
+
+interface ActionTooltipProps { 
+  label: string;
+  children: React.ReactNode;
+  side?: "left" | "right" | "top" | "bottom";
+  align?: "start" | "center" | "end";
+}
+
+const ActionTooltip = ({ label, children, side, align}:ActionTooltipProps) => {
+  return ( 
+    <TooltipProvider>
+      <Tooltip delayDuration={50}>
+        <TooltipTrigger asChild>
+          { children}
+        </TooltipTrigger>
+        <TooltipContent side={side} align={ align}>
+          <p className="font-semibold text-sm capitalize">
+            { label }
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
+ 
+export default ActionTooltip;
